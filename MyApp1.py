eb829f91ac7989d70242b83e0e9f1f9c1b4e1dbb
sopidasri <nuunanni@gmail.com>
2024-03-28 12:06:28 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -24,9 +24,15 @@ cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['petal.length'].sum())
 cl4.write(dt['petal.width'].sum())
 
-cols=['sepal.length', 'sepal.width','petal.length']
-dx=dt[cols]
-st.bar_chart(dx,x='sepal.length',y='sepal.width',color='petal.length')
+#cols=['sepal.length', 'sepal.width','petal.length']
+dx=pd.DataFrame(
+    {
+        "col1":dt['sepal.length'].sum(),
+        "col2":dt['sepal.width'].sum(),
+        "col3":dt['petal.length'].sum()
+    }
+)
+st.bar_chart(dx,x='col1',y='col3',color='col3')
 
 st.write('ค่าเฉลี่ย')
 cl11,cl12,cl13,cl14=st.columns(4)
