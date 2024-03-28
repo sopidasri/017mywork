fa5dddef6d5f9629341ac0373e5616f9eb3e360c
sopidasri <nuunanni@gmail.com>
2024-03-28 11:29:41 +0700

    show data

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Website Developing using Python")
 st.header("🍥Website Developing using Python🍥")
 st.subheader("Sopida Srinoy🍌")
 st.image('pic.jpg')
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
