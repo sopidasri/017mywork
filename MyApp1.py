75255263e38b93117c33d5e69eac9bd7196ec2c4
sopidasri <nuunanni@gmail.com>
2024-03-28 11:14:55 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Website Developing usinf Python")
 st.header("Website Developing usinf Python")
 st.subheader("Sopida Srinoy")
-st.img('pic.jpg')
+st.image('pic.jpg')
